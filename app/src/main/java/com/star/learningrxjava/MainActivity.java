95708d42a6e5cc8d2409f147deec31215a0c9add
crystalDf <chendong333@gmail.com>
2018-04-03 22:19:55 +0800

    LearningRxJava

---
+++ app/src/main/java/com/star/learningrxjava/MainActivity.java
@@ -0,0 +1,52 @@
+package com.star.learningrxjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "RxJava";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Observable<Integer> observable = Observable.create(emitter -> {
+
+            emitter.onNext(1);
+            emitter.onNext(2);
+            emitter.onNext(3);
+            emitter.onComplete();
+        });
+
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "开始采用subscribe连接");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "对Next事件"+ integer +"作出响应"  );
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "对Error事件作出响应");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "对Complete事件作出响应");
+            }
+        };
+
+        observable.subscribe(observer);
+    }
+}
