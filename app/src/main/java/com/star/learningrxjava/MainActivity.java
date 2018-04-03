ee72f017931c755d3f06cfe5baa7e784642a6a87
crystalDf <chendong333@gmail.com>
2018-04-03 22:35:30 +0800

    LearningRxJava

--- app/src/main/java/com/star/learningrxjava/MainActivity.java
+++ app/src/main/java/com/star/learningrxjava/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
@@ -48,5 +49,44 @@ public class MainActivity extends AppCompatActivity {
         };
 
         observable.subscribe(observer);
+
+        Observable.create((ObservableOnSubscribe<Integer>) emitter -> {
+
+            emitter.onNext(1);
+            emitter.onNext(2);
+            emitter.onNext(3);
+            emitter.onComplete();
+        }).subscribe(new Observer<Integer>() {
+
+            private Disposable mDisposable;
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "开始采用subscribe连接");
+
+                mDisposable = d;
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "对Next事件"+ integer +"作出响应"  );
+
+                if (integer == 2) {
+                    mDisposable.dispose();
+
+                    Log.d(TAG, "已经切断了连接：" + mDisposable.isDisposed());
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "对Error事件作出响应");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "对Complete事件作出响应");
+            }
+        });
     }
 }
